560979be642783cafa87945d44a6801c703e0ae6
Rocky-VIV <rgent2@illinois.edu>
2026-06-29 18:06:04 -0500

    Rename MUX8 to MUX28 and update ranges
    Need to update arduino sketch with new boundaries since I adopted the triple digit range.

--- src/elab/MUX8.py
+++ src/elab/MUX8.py
@@ -1,37 +1,36 @@
 from .main import instrument
 
-class MUX8(instrument):
+class MUX28(instrument):
 
     def __init__(self, com_port, **kwargs):
         super().__init__(com_port, baud_rate=115200, **kwargs)
-        self.model = 'Arduino MUX'
+        self.model = 'Arduino MUX 28'
         self.type = 'MUX'
         if self.verbose == True:
             print(f'{self.model} connected on {com_port} at {self.baud_rate} bits/s')
 
     def electrode(self,n):
-        n += 10
-        if n not in range(11,19):
-            raise ValueError('Electrode n must be between 1 and 8')
-        self.send_comm(n)
+        if n not in range(1,29):
+            raise ValueError('Electrode n must be between 1 and 28')
+        self.send_comm(n+100)
         
     def ida(self,n):
-        if n not in range(1,5):
-            raise ValueError('IDA n must be between 1 and 4')
+        if n not in range(1,15):
+            raise ValueError('IDA n must be between 1 and 14')
         self.send_comm(n)
 
     def gen(self,n):
-        n_gen = [14,13,12,11]
-        if n not in range(1,5):
-            raise ValueError('Generator n must be between 1 and 4')
-        self.send_comm(n_gen[n-1])
+        if n not in range(1,15):
+            raise ValueError('Generator n must be between 1 and 14')
+        command = (115-n)
+        self.send_comm(command)
 
     def coll(self,n):
-        n_coll = [15,16,17,18]
-        if n not in range(1,5):
-            raise ValueError('Collector n must be between 1 and 4')
-        self.send_comm(n_coll[n-1])
-
+        if n not in range(1,15):
+            raise ValueError('Collector n must be between 1 and 14')
+        command = (114+n)
+        self.send_comm(command)
+        
     def send_comm(self,n):
         self.ser.write(bytes(f'<{n}>', 'utf-8'))
         msg = self.ser.readline()
@@ -39,10 +38,10 @@ class MUX8(instrument):
             print(msg.decode('utf-8'))
 
     def gen_all(self):
-        self.send_comm(30)
+        self.send_comm(230)
     
     def coll_all(self):
-        self.send_comm(20)
+        self.send_comm(220)
 
     def all(self):
-        self.send_comm(40)
+        self.send_comm(240)
